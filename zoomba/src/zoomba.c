674f76f5b56e23138841cae81666b22de7e67e10
lasedini <panmakagr25@gmail.com>
2024-02-03 21:40:59 +0200

    no comments

--- zoomba/src/zoomba.c
+++ zoomba/src/zoomba.c
@@ -15,7 +15,6 @@ typedef struct tree{
     struct tree * right;
     struct tree * left;
     struct tree * previous;
-    //char data;
 }tree;
 
 typedef struct queue{
@@ -25,16 +24,14 @@ typedef struct queue{
 
 int existence(char **visited, int h, int v) {// horizontal, vertical
     if (h < 0 || v < 0 || h >= dim || v >= dim) return 0;
-    else if(visited[h][v])return 0;
+    else if(visited[h][v]=='1')return 0;
     else return 1;
 }
-
 void add_node(tree *p, int U, int R, int D, int L, int row, int collumn, char **visited, queue **tail){
     tree *previous;
-    printf("hello\n");
     previous=p;
     if(U==1)
-    {
+    {   
         p=p->up;
         p=(tree*) malloc(sizeof(tree));
         if (p==NULL){
@@ -46,7 +43,7 @@ void add_node(tree *p, int U, int R, int D, int L, int row, int collumn, char **
         p->row=row-1;
         p->collumn=collumn;
         p->key=visited[p->row][p->collumn];
-        visited[p->row][p->collumn]=1;
+        visited[p->row][p->collumn]='1';
         queue * new_node;
         new_node=(queue*)malloc(sizeof(queue));
         if(new_node==NULL){
@@ -59,7 +56,7 @@ void add_node(tree *p, int U, int R, int D, int L, int row, int collumn, char **
         new_node->next=NULL;
     }
     if(R==1)
-    {
+    {   
         p=p->right;
         p=(tree*) malloc(sizeof(tree));
         if (p==NULL){
@@ -71,7 +68,7 @@ void add_node(tree *p, int U, int R, int D, int L, int row, int collumn, char **
         p->row=row;
         p->collumn=collumn+1;
         p->key=visited[p->row][p->collumn];
-        visited[p->row][p->collumn]=1;
+        visited[p->row][p->collumn]='1';
         queue * new_node;
         new_node=(queue*)malloc(sizeof(queue));
         if(new_node==NULL){
@@ -84,7 +81,7 @@ void add_node(tree *p, int U, int R, int D, int L, int row, int collumn, char **
         new_node->next=NULL;
     }
     if(D==1)
-    {
+    {   
         p=p->down;
         p=(tree*) malloc(sizeof(tree));
         if (p==NULL){
@@ -96,7 +93,7 @@ void add_node(tree *p, int U, int R, int D, int L, int row, int collumn, char **
         p->row=row+1;
         p->collumn=collumn;
         p->key=visited[p->row][p->collumn];
-        visited[p->row][p->collumn]=1;
+        visited[p->row][p->collumn]='1';
         queue * new_node;
         new_node=(queue*)malloc(sizeof(queue));
         if(new_node==NULL){
@@ -109,7 +106,7 @@ void add_node(tree *p, int U, int R, int D, int L, int row, int collumn, char **
         new_node->next=NULL;
     }
     if(L==1)
-    {
+    {   
         p=p->left;
         p=(tree*) malloc(sizeof(tree));
         if (p==NULL){
@@ -121,7 +118,7 @@ void add_node(tree *p, int U, int R, int D, int L, int row, int collumn, char **
         p->row=row;
         p->collumn=collumn-1;
         p->key=visited[p->row][p->collumn];
-        visited[p->row][p->collumn]=1;
+        visited[p->row][p->collumn]='1';
         queue * new_node;
         new_node=(queue*)malloc(sizeof(queue));
         if(new_node==NULL){
@@ -147,8 +144,6 @@ void delete_tree(tree *p){
 void find_path(char **visited,int RowZ, int ColZ){
     tree * root;
     char key=visited[RowZ][ColZ];
-    //char direction;
-    //root =NULL;
     root=(tree*) malloc(sizeof(tree));
         if (root==NULL){
             perror("Not available memory\n");
@@ -157,7 +152,6 @@ void find_path(char **visited,int RowZ, int ColZ){
     root->up = root->down = root->left = root->right =NULL;
     root->key=key;
     root->previous=NULL;
-    //root->data='\0';
     root->row=RowZ;
     root->collumn=ColZ;
     
@@ -175,25 +169,26 @@ void find_path(char **visited,int RowZ, int ColZ){
     int U,R,D,L;
     tree *navigator=NULL;
     while(head!=NULL){//check_add_and_pop and fill the corresponding pop in visited array with 1 if we didnt find the trash
-        if(head->pointer->key==2){
+        if(head->pointer->key=='2'){
             navigator=head->pointer;
             break;
         }
         U=R=D=L=0;
+        int adjx;
+        int adjy;
         for (int i = 0; i < 4; i++) {//to find what positions are free to fill the tree
-        int adjx = tail->pointer->row + DirR[i];
-        int adjy = tail->pointer->collumn + DirC[i];
-        if (existence(visited, adjx, adjy)) {
-           
-        if(i==0){ U=1;}
-        if(i==1){ R=1;}
-        if(i==2){ D=1;}
-        if(i==3){ L=1;}
+        adjx = head->pointer->row;
+        adjy = head->pointer->collumn;
+        adjx = head->pointer->row + DirR[i];
+        adjy = head->pointer->collumn + DirC[i];
+        if (existence(visited, adjx, adjy)) { 
+            if(i==0) U=1;
+            if(i==1) R=1;
+            if(i==2) D=1;
+            if(i==3) L=1;
+            }
         }
-        }
-        add_node(tail->pointer, U, R, D, L, tail->pointer->row, tail->pointer->collumn, visited, &tail);
-        
-        //pop the node we checked from the queue
+        add_node(head->pointer, U, R, D, L, head->pointer->row, head->pointer->collumn, visited, &tail);
         temp_head=head->next;
         free(head);
         head=temp_head;
@@ -202,8 +197,8 @@ void find_path(char **visited,int RowZ, int ColZ){
     else{
         char *path=NULL;
         int counter=0;
-        while(navigator!=NULL){
-            path=realloc(path, counter+1);
+        while(navigator!=root){
+            path=realloc(path, sizeof(char)*(counter+1));
             if (path==NULL){
                 perror("Not available memory\n");
                 exit(1);
@@ -224,6 +219,8 @@ void find_path(char **visited,int RowZ, int ColZ){
         }
         printf("\n");
         free(path);
+       
+
     }   
     //free the queue
     while(head!=NULL){
@@ -246,9 +243,8 @@ int main(void) {
     if (!(fscanf(stdin, "%d", &poszoo_y))) exit(1);
     if (!(fscanf(stdin, "%d", &trash_x))) exit(1);
     if (!(fscanf(stdin, "%d", &trash_y))) exit(1);
-    char t;
-    if (!(fscanf(stdin, "%c", &t))) exit(1);
-    printf("%d %d %d %d %d\n", dim, poszoo_x, poszoo_y, trash_x, trash_y);
+    char n;
+    if (!(fscanf(stdin, "%c", &n))) exit(1);
     if (dim > 10000) exit(1); //checking if the dimensions exceed the allowed boundaries 
     if (poszoo_x >= dim || poszoo_y >= dim || trash_x >= dim || trash_y >= dim) exit(1); //checking if the position of the machine or the trash is out of bounds
     if (poszoo_x < 0 || poszoo_y < 0 || trash_x < 0 || trash_y < 0) exit(1);
@@ -258,23 +254,18 @@ int main(void) {
             room[i] = malloc(dim * sizeof(char));
             if (room[i] == NULL) exit(1);
         }
+    char * temp=(char *)malloc(dim*sizeof(char));
     for (int i = 0; i < dim; i++) {
         int read = fread(room[i], 1, dim, stdin);
         if (read != dim) exit(1);
-        char *temp;
-        if(!(fread(temp, 1, 1, stdin))) exit(1);  
+        if(!(fread(temp,1,1,stdin))) exit(1);    
     }
-    //if (feof(stdin) == EOF) exit(1);
+    if (feof(stdin) == EOF) exit(1);
     if (room[poszoo_x][poszoo_y] == '1' || room[trash_x][trash_y] == '1') exit(1); //checking if the machine or the trash is on top of an obstacle
-    if  (poszoo_x == trash_x && poszoo_y == trash_y){
-        fprintf(stderr, "No steps needed. Zoomba is on the trash");
+    if  (&room[poszoo_x][poszoo_y] == &room[trash_x][trash_y]){
+        printf("No steps needed. Zoomba is on the trash\n");
         return 0;
     }
-    // for (int i = 0; i < dim; i++) {
-    //     for (int j = 0; j < dim; j++) {
-    //         printf("%c", room[i][j]);
-    //     }
-    // }
    
     char **visited = malloc(dim * sizeof(char*)); //introducing a 2-dimensional array that marks if a cell has been visited or not
     for (int i = 0; i < dim; i++) {
@@ -285,17 +276,7 @@ int main(void) {
     }
     visited[trash_x][trash_y] = '2';//mark the trash as 2
     visited[poszoo_x][poszoo_y] = '1';//mark zoomba as 1
-    for (int i = 0; i < dim; i++) {
-        for (int j = 0; j < dim; j++) {
-            printf("%c", visited[i][j]);
-        }
-        printf("\n");
-    }
-
-    //find_path(visited,poszoo_x,poszoo_y);
-
-
-
+    find_path(visited,poszoo_x,poszoo_y);
     for (int i = 0; i < dim; i++) { //freeing the two-dimensional array of the room
         free(visited[i]);
     }
