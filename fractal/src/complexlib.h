53731f77358cc8ecaa18aad9588f55792d78bc28
lasedini <panmakagr25@gmail.com>
2024-02-06 00:19:38 +0200

    figuring it out

---
+++ fractal/src/complexlib.h
@@ -0,0 +1,19 @@
+double imadd(double a, double b) {
+
+}
+
+double imsub(double a, double b) {
+
+}
+
+double immul(double a, double b) {
+
+}
+
+double imdiv(double a, double b) {
+
+}
+
+double imabs(double a, double b) {
+    
+}
--- fractal/src/fractal.c
+++ fractal/src/fractal.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include "complexlib.h"
 
 typedef struct {
     double real;
     double imag;
 } complex;
+
+typedef struct {
+    complex next;
+    complex now;
+}Z;
 
 int main(int argc, char **argv) {
     if (argc != 2) {
@@ -18,12 +25,32 @@ int main(int argc, char **argv) {
     }
     int degree;
     if (!(fscanf(comp, "%d", &degree))) exit(1);
+    if (degree > 10) exit(1);
     double *coef = malloc((degree + 1) * sizeof(double));
-    for (int i = 0; i < (degree + 1); i ++) {
+    for (int i = 0; i <= degree; i ++) {
         if(!(fscanf(comp, "%lf", &coef[i]))) exit(1);
     }
     complex min, max;
-    if (!(fscanf(comp, "%lf %lf %lf %lf", min.real, min.imag, max.real, max.imag))) exit(1);
+    if (!(fscanf(comp, "%lf %lf %lf %lf", &min.real, &min.imag, &max.real, &max.imag))) exit(1);
+    double step;
+    if (!(fscanf(comp, "%lf", &step))) exit(1);
+    Z z;
+    int count = 0;
+    for (double i = min.imag; i <= max.imag; i+=step) {
+        z.now.imag = i;
+        for (double j = min.real; j <= max.real; j+=step) {
+            z.now.real = j;
+            do {
+
+            } while (imabs(z.now.imag, z.now.real) >= 10e-6 && count < 1000);
+            if (count == 1000) {
+                printf("impossible\t");
+                continue;
+            }
+            printf("%.2lf %.2lfi\t", z.now.imag, z.now.real);
+        }
+        printf("\n");
+    }
     fclose(comp);
     return 0;
-} 
+}
